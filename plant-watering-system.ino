1ee3b83bedc1b17de1cda2f8aec076b9d711da44
rkr <ron.kirschler@gmail.com>
2023-06-17 21:26:16 +0200

    Removed motion sensor code and some other changes

--- plant-watering-system.ino
+++ plant-watering-system.ino
@@ -1,9 +1,9 @@
 #include <TaskManagerIO.h>
 
-#define PIN_MOTION_SENSOR 2
 #define PIN_PUMP 3
-#define PIN_SPEAKER 4
 #define PIN_LED 5
+
+#define PIN_WATER_LEVEL A0
 #define PIN_MOISTURE_SENSOR A2
 
 #define MOISTURE_FULL_DRY 530
@@ -12,12 +12,13 @@
 #define MOISTURE_DRY MOISTURE_FULL_DRY - MOISTURE_INTERVAL
 #define MOISTURE_WET MOISTURE_FULL_WET + MOISTURE_INTERVAL
 
-enum MoistureLevel { DRY, MOIST, WET };
+#define WATER_LEVEL_THRESHOLD 750
+
+enum MoistureLevel { SOIL_DRY, SOIL_MOIST, SOIL_WET };
 enum PumpState { PUMP_INIT, PUMP_CHECK, PUMP_WAIT, PUMP_ACTIVE, PUMP_IDLE, PUMP_OFF_BLINK_OFF, PUMP_OFF_BLINK_ON };
 enum WaterState { WATER_INIT, WATER_OK, WATER_LOW };
 
 WaterState waterState = WATER_INIT;
-long motionDetected = 0;
 
 class PumpStateHandler {
   private:
@@ -28,7 +29,7 @@ class PumpStateHandler {
   public:
     PumpState state = PUMP_INIT;
 
-    void wait(int _millis, PumpState nextState) {
+    void wait(unsigned long _millis, PumpState nextState) {
       nextMillis = _millis + millis();
       this->nextState = nextState;
       this->active = true;
@@ -44,7 +45,7 @@ class PumpStateHandler {
     }
 };
 
-MoistureLevel moistureLevel = WET;
+MoistureLevel moistureLevel = SOIL_WET;
 PumpStateHandler* pumpStateHandler = new PumpStateHandler();
 
 void checkMoistureLevel() {
@@ -54,35 +55,35 @@ void checkMoistureLevel() {
   Serial.print("; Soil is ");
   if(moistureSensorValue > MOISTURE_DRY) {
     Serial.println("dry.");
-    moistureLevel = DRY;
+    moistureLevel = SOIL_DRY;
   } else if(moistureSensorValue > MOISTURE_WET) {
     Serial.println("wet.");
-    moistureLevel = MOIST;
+    moistureLevel = SOIL_MOIST;
   } else {
     Serial.println("very wet.");
-    moistureLevel = WET;
+    moistureLevel = SOIL_WET;
   }
 }
 
 void checkWaterLevel() {
   int checkWaterLevel;
 
+  int level2 = 1024;
+  for(int i = 0; i < 20; i++) {
+    level2 = min(analogRead(PIN_WATER_LEVEL), level2);
+    delay(2);
+  }
+
+  Serial.print("Variable_3:");
+  Serial.println(level2);
+
   if(waterState == WATER_LOW) {
     return;
   }
 
-  int level2 = 1024;
-  for(int i = 0; i < 20; i++) {
-    level2 = min(analogRead(A1), level2);
-    delay(2);
-  }
-
-  waterState = level2 > 150 ? WATER_OK : WATER_LOW;
+  waterState = level2 > WATER_LEVEL_THRESHOLD ? WATER_OK : WATER_LOW;
 
   pumpStateHandler->state = PUMP_CHECK;
-
-  Serial.print("Variable_1:");
-  Serial.println(level2 > 100 ? 1000 : 0);
 }
 
 void handlePumpState() {
@@ -96,12 +97,12 @@ void handlePumpState() {
 
   if(pumpStateHandler->state == PUMP_OFF_BLINK_OFF) {
     digitalWrite(PIN_LED, LOW);
-    return pumpStateHandler->wait(750, PUMP_OFF_BLINK_ON);
+    return pumpStateHandler->wait(750UL, PUMP_OFF_BLINK_ON);
   }
 
   if(pumpStateHandler->state == PUMP_OFF_BLINK_ON) {
     digitalWrite(PIN_LED, HIGH);
-    return pumpStateHandler->wait(750, PUMP_OFF_BLINK_OFF);
+    return pumpStateHandler->wait(750UL, PUMP_OFF_BLINK_OFF);
   }
 
   if(pumpStateHandler->state == PUMP_CHECK) {
@@ -110,52 +111,37 @@ void handlePumpState() {
       return;
     }
 
-    if(moistureLevel == DRY) {
+    if(moistureLevel == SOIL_DRY) {
       pumpStateHandler->state = PUMP_ACTIVE;
       return;
     }
 
-    return pumpStateHandler->wait(5000, PUMP_CHECK);
+    return pumpStateHandler->wait(5000UL, PUMP_CHECK);
   }
   
   if(pumpStateHandler->state == PUMP_ACTIVE) {
+    Serial.print("Variable_2:");
+    Serial.println(500);
     digitalWrite(PIN_PUMP, HIGH);
-    return pumpStateHandler->wait(2500, PUMP_IDLE);
+    return pumpStateHandler->wait(2500UL, PUMP_IDLE);
   }
   
   if(pumpStateHandler->state == PUMP_IDLE) {
+    Serial.print("Variable_2:");
+    Serial.println(0);
     digitalWrite(PIN_PUMP, LOW);
-    return pumpStateHandler->wait(10000, PUMP_CHECK);
-  }
-}
-
-void checkMotionSensor() {
-  if(waterStats == WATER_OK) {
-    return;
-  }
-
-  int sensor = digitalRead(PIN_MOTION_SENSOR);
-  if(sensor > 0 && motionDetected == 0) {
-    motionDetected = millis() + 1000 * 1;
-  }
-  if(motionDetected > millis()) {
-    tone(PIN_SPEAKER, 1000 - (motionDetected - millis()) / 3);
-  } else {
-    noTone(PIN_SPEAKER);
+    return pumpStateHandler->wait(10000UL, PUMP_CHECK);
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PIN_MOTION_SENSOR, INPUT);
   pinMode(PIN_PUMP, OUTPUT);
-  pinMode(PIN_SPEAKER, OUTPUT);
   pinMode(PIN_LED, OUTPUT);
 
   taskManager.scheduleFixedRate(1000, checkMoistureLevel);
   taskManager.scheduleFixedRate(500, checkWaterLevel);
   taskManager.scheduleFixedRate(50, handlePumpState);
-  taskManager.scheduleFixedRate(50, checkMotionSensor);
 }
 
 void loop() {
