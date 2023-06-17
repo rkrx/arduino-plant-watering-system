509c8b75407e703775c0fe281e60fe19ef186ad4
rkr <ron.kirschler@gmail.com>
2023-06-17 16:01:36 +0200

    Motion sensor triggers sound at low water level.

--- auto-water.ino
+++ auto-water.ino
@@ -1,7 +1,10 @@
 #include <TaskManagerIO.h>
 
+#define PIN_MOTION_SENSOR 2
 #define PIN_PUMP 3
+#define PIN_SPEAKER 4
 #define PIN_LED 5
+#define PIN_MOISTURE_SENSOR A2
 
 #define MOISTURE_FULL_DRY 530
 #define MOISTURE_FULL_WET 240
@@ -9,18 +12,21 @@
 #define MOISTURE_DRY MOISTURE_FULL_DRY - MOISTURE_INTERVAL
 #define MOISTURE_WET MOISTURE_FULL_WET + MOISTURE_INTERVAL
 
-bool waterOk = true;
 enum MoistureLevel { DRY, MOIST, WET };
-enum PumpState { INIT, CHECK, WAIT, PUMP, IDLE, PUMP_OFF_BLINK_OFF, PUMP_OFF_BLINK_ON };
+enum PumpState { PUMP_INIT, PUMP_CHECK, PUMP_WAIT, PUMP_ACTIVE, PUMP_IDLE, PUMP_OFF_BLINK_OFF, PUMP_OFF_BLINK_ON };
+enum WaterState { WATER_INIT, WATER_OK, WATER_LOW };
+
+WaterState waterState = WATER_INIT;
+long motionDetected = 0;
 
 class PumpStateHandler {
   private:
     unsigned long nextMillis = 0;
-    PumpState nextState = INIT;
+    PumpState nextState = PUMP_INIT;
     bool active = false;
   
   public:
-    PumpState state = INIT;
+    PumpState state = PUMP_INIT;
 
     void wait(int _millis, PumpState nextState) {
       nextMillis = _millis + millis();
@@ -42,7 +48,7 @@ MoistureLevel moistureLevel = WET;
 PumpStateHandler* pumpStateHandler = new PumpStateHandler();
 
 void checkMoistureLevel() {
-  int moistureSensorValue = analogRead(A2);
+  int moistureSensorValue = analogRead(PIN_MOISTURE_SENSOR);
   Serial.print("Current moisture level: ");
   Serial.print(moistureSensorValue);
   Serial.print("; Soil is ");
@@ -61,7 +67,7 @@ void checkMoistureLevel() {
 void checkWaterLevel() {
   int checkWaterLevel;
 
-  if(!waterOk) {
+  if(waterState == WATER_LOW) {
     return;
   }
 
@@ -71,9 +77,9 @@ void checkWaterLevel() {
     delay(2);
   }
 
-  waterOk = level2 > 150;
+  waterState = level2 > 150 ? WATER_OK : WATER_LOW;
 
-  pumpStateHandler->state = CHECK;
+  pumpStateHandler->state = PUMP_CHECK;
 
   Serial.print("Variable_1:");
   Serial.println(level2 > 100 ? 1000 : 0);
@@ -84,7 +90,7 @@ void handlePumpState() {
     return;
   }
 
-  if(pumpStateHandler->state == INIT) {
+  if(pumpStateHandler->state == PUMP_INIT) {
     return;
   }
 
@@ -98,39 +104,58 @@ void handlePumpState() {
     return pumpStateHandler->wait(750, PUMP_OFF_BLINK_OFF);
   }
 
-  if(pumpStateHandler->state == CHECK) {
-    if(!waterOk) {
+  if(pumpStateHandler->state == PUMP_CHECK) {
+    if(waterState != WATER_OK) {
       pumpStateHandler->state = PUMP_OFF_BLINK_ON;
       return;
     }
 
     if(moistureLevel == DRY) {
-      pumpStateHandler->state = PUMP;
+      pumpStateHandler->state = PUMP_ACTIVE;
       return;
     }
 
-    return pumpStateHandler->wait(5000, CHECK);
+    return pumpStateHandler->wait(5000, PUMP_CHECK);
   }
   
-  if(pumpStateHandler->state == PUMP) {
+  if(pumpStateHandler->state == PUMP_ACTIVE) {
     digitalWrite(PIN_PUMP, HIGH);
-    return pumpStateHandler->wait(2500, IDLE);
+    return pumpStateHandler->wait(2500, PUMP_IDLE);
   }
   
-  if(pumpStateHandler->state == IDLE) {
+  if(pumpStateHandler->state == PUMP_IDLE) {
     digitalWrite(PIN_PUMP, LOW);
-    return pumpStateHandler->wait(10000, CHECK);
+    return pumpStateHandler->wait(10000, PUMP_CHECK);
+  }
+}
+
+void checkMotionSensor() {
+  if(waterStats == WATER_OK) {
+    return;
+  }
+
+  int sensor = digitalRead(PIN_MOTION_SENSOR);
+  if(sensor > 0 && motionDetected == 0) {
+    motionDetected = millis() + 1000 * 1;
+  }
+  if(motionDetected > millis()) {
+    tone(PIN_SPEAKER, 1000 - (motionDetected - millis()) / 3);
+  } else {
+    noTone(PIN_SPEAKER);
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(PIN_MOTION_SENSOR, INPUT);
+  pinMode(PIN_PUMP, OUTPUT);
+  pinMode(PIN_SPEAKER, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
 
   taskManager.scheduleFixedRate(1000, checkMoistureLevel);
   taskManager.scheduleFixedRate(500, checkWaterLevel);
   taskManager.scheduleFixedRate(50, handlePumpState);
+  taskManager.scheduleFixedRate(50, checkMotionSensor);
 }
 
 void loop() {
