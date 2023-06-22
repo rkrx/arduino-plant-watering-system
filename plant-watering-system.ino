1e4ad6d71f7098e7edf7935059cc76bf29018db1
Ron Kirschler <ron.kirschler@gmail.com>
2023-06-22 08:46:03 +0200

    Update plant-watering-system.ino

--- plant-watering-system.ino
+++ plant-watering-system.ino
@@ -1,6 +1,7 @@
 #include <TaskManagerIO.h>
 #include <LiquidCrystal.h>
 
+//region Constants
 #define PIN_PUMP 3
 #define PIN_SOIL_SIGNAL 4
 #define PIN_LED 5
@@ -23,29 +24,44 @@
 #define VAR_MOISTURE "Moisture"
 #define VAR_PUMP "Pump"
 
-enum MoistureLevel { SOIL_DRY, SOIL_MOIST, SOIL_WET };
-enum PumpState { PUMP_INIT, PUMP_CHECK, PUMP_WAIT, PUMP_ACTIVE, PUMP_IDLE, PUMP_OFF_BLINK_OFF, PUMP_OFF_BLINK_ON };
-enum WaterState { WATER_INIT, WATER_OK, WATER_LOW };
-
 #define LCD_RS_PIN 12
 #define LCD_EN_PIN 11
 #define LCD_D4_PIN 10
 #define LCD_D5_PIN 9
 #define LCD_D6_PIN 8
 #define LCD_D7_PIN 7
+//endregion
+
+enum MoistureLevel { SOIL_DRY, SOIL_MOIST, SOIL_WET };
+enum PumpState { PUMP_INIT, PUMP_CHECK, PUMP_WAIT, PUMP_ACTIVE, PUMP_IDLE, PUMP_OFF_BLINK_OFF, PUMP_OFF_BLINK_ON };
+enum WaterState { WATER_INIT, WATER_OK, WATER_LOW };
 
 LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
 WaterState waterState = WATER_INIT;
 
+long pumpCount = 0;
+const byte moistureSensorValueCount = 10;
+int moistureSensorValues[moistureSensorValueCount] = { MOISTURE_DRY - MOISTURE_INTERVAL / 2 };
+
 class PumpStateHandler {
   private:
     unsigned long nextMillis = 0;
     PumpState nextState = PUMP_INIT;
+    PumpState state = PUMP_INIT;
     bool active = false;
   
   public:
-    PumpState state = PUMP_INIT;
+    PumpState getState() {
+      return state;
+    }
+
+    setState(PumpState state) {
+      if(this->state == PUMP_ACTIVE && state != PUMP_ACTIVE) {
+        pumpCount++;
+      }
+      this->state = state;
+    }
 
     void wait(unsigned long _millis, PumpState nextState) {
       nextMillis = _millis + millis();
@@ -70,8 +86,17 @@ PumpStateHandler* pumpStateHandler = new PumpStateHandler();
 
 void checkMoistureLevel() {
   if(moistureIdleTimeout < millis()) {
-    moistureSensorValue = analogRead(PIN_MOISTURE_SENSOR);
+    int newMoistureSensorValue = analogRead(PIN_MOISTURE_SENSOR);
     moistureIdleTimeout = millis() + 1500UL;
+
+    int total = 0;
+    for(int i = moistureSensorValueCount - 2; i >= 0; i--) {
+    moistureSensorValues[i + 1] = moistureSensorValues[i];
+      total += moistureSensorValues[i];
+    }
+    moistureSensorValues[0] = newMoistureSensorValue; 
+
+    moistureSensorValue = total / moistureSensorValueCount;
 
     Serial.print(VAR_MOISTURE);
     Serial.print(":");
@@ -117,27 +142,27 @@ void handlePumpState() {
     return;
   }
 
-  if(pumpStateHandler->state == PUMP_INIT) {
+  if(pumpStateHandler->getState() == PUMP_INIT) {
     if(waterState == WATER_OK) {
-      pumpStateHandler->state = PUMP_CHECK;
+      pumpStateHandler->setState(PUMP_CHECK);
     } else {
       return;
     }
   }
 
-  if(pumpStateHandler->state == PUMP_OFF_BLINK_OFF) {
+  if(pumpStateHandler->getState() == PUMP_OFF_BLINK_OFF) {
     digitalWrite(PIN_LED, LOW);
     return pumpStateHandler->wait(750UL, PUMP_OFF_BLINK_ON);
   }
 
-  if(pumpStateHandler->state == PUMP_OFF_BLINK_ON) {
+  if(pumpStateHandler->getState() == PUMP_OFF_BLINK_ON) {
     digitalWrite(PIN_LED, HIGH);
     return pumpStateHandler->wait(750UL, PUMP_OFF_BLINK_OFF);
   }
 
-  if(pumpStateHandler->state == PUMP_CHECK) {
+  if(pumpStateHandler->getState() == PUMP_CHECK) {
     if(waterState != WATER_OK) {
-      pumpStateHandler->state = PUMP_OFF_BLINK_ON;
+      pumpStateHandler->setState(PUMP_OFF_BLINK_ON);
       return;
     }
 
@@ -152,20 +177,20 @@ void handlePumpState() {
     }*/
 
     if(moistureLevel == SOIL_DRY) {
-      pumpStateHandler->state = PUMP_ACTIVE;
+      pumpStateHandler->setState(PUMP_ACTIVE);
       return;
     }
 
     return pumpStateHandler->wait(5000UL, PUMP_CHECK);
   }
   
-  if(pumpStateHandler->state == PUMP_ACTIVE) {
+  if(pumpStateHandler->getState() == PUMP_ACTIVE) {
     Serial.println(sprintf("%s:%d", VAR_PUMP, 500));
     digitalWrite(PIN_PUMP, HIGH);
     return pumpStateHandler->wait(PUMP_ACTIVE_TIME, PUMP_IDLE);
   }
   
-  if(pumpStateHandler->state == PUMP_IDLE) {
+  if(pumpStateHandler->getState() == PUMP_IDLE) {
     Serial.println(sprintf("%s:%d", VAR_PUMP, 0));
     digitalWrite(PIN_PUMP, LOW);
     return pumpStateHandler->wait(PUMP_IDLE_TIME, PUMP_CHECK);
@@ -179,31 +204,29 @@ void updateDisplay() {
   lcd.print((moistureSensorValue - MOISTURE_WET) * 100 / (MOISTURE_DRY - MOISTURE_WET));
   lcd.print("% DRY, ");
   
-  if(pumpStateHandler->state == PUMP_INIT) {
+  if(pumpStateHandler->getState() == PUMP_INIT) {
     lcd.print("INIT");
-  } else if(pumpStateHandler->state == PUMP_WAIT) {
+  } else if(pumpStateHandler->getState() == PUMP_WAIT) {
     lcd.print("WAIT");
-  } else if(pumpStateHandler->state == PUMP_ACTIVE) {
+  } else if(pumpStateHandler->getState() == PUMP_ACTIVE) {
     lcd.print("ACTIVE");
-  } else if(pumpStateHandler->state == PUMP_CHECK) {
+  } else if(pumpStateHandler->getState() == PUMP_CHECK) {
     lcd.print("CHECK");
-  } else if(pumpStateHandler->state == PUMP_IDLE) {
+  } else if(pumpStateHandler->getState() == PUMP_IDLE) {
     lcd.print("IDLE");
-  } else if(pumpStateHandler->state == PUMP_IDLE_TIME) {
-    lcd.print("???");
-  } else if(pumpStateHandler->state == PUMP_ACTIVE_TIME) {
-    lcd.print("???");
-  } else if(pumpStateHandler->state == PUMP_OFF_BLINK_OFF || pumpStateHandler->state == PUMP_OFF_BLINK_ON) {
+  } else if(pumpStateHandler->getState() == PUMP_OFF_BLINK_OFF || pumpStateHandler->getState() == PUMP_OFF_BLINK_ON) {
     lcd.print("OFF");
   }
 
   lcd.setCursor(0, 1);
-  lcd.print("Water=");
+  lcd.print("Water ");
   if(waterState == WATER_OK) {
     lcd.print("OK");
   } else {
     lcd.print("Empty");
   }
+  lcd.print(", Pump: ");
+  lcd.print(pumpCount);
 }
 
 void setup() {
